0947b515880c8f917fb29c055927cc68994c5cdc
nwagu chima <chima.nwagu@decagon.dev>
2022-06-10 22:47:56 +0100

    implemented delete query and also optimized cache

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutaitions";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.filter((c) => c.id !== deleteClient.id) },
+      });
+    },
+  });
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-sm btn-danger">
+        <button className="btn btn-sm btn-danger" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
