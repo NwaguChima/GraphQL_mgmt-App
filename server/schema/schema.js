3030e48b983b74f357a91cc693e5c42e3ff21f0b
nwagu chima <chima.nwagu@decagon.dev>
2022-06-10 11:28:06 +0100

    connect graphql to mongoose to fetch data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 const { projects, clients } = require("../sampleData");
 
+const Project = require("../models/Project");
+const Client = require("../models/Client");
+
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -30,7 +33,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.client);
       },
     },
   }),
@@ -42,7 +45,7 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        Project.find();
       },
     },
     project: {
@@ -51,14 +54,14 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLID },
       },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
 
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
@@ -67,7 +70,7 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLID },
       },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
   },
