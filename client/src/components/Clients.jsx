0bb56bad1fa4ab3746743445b74f0283bb7eaf64
nwagu chima <chima.nwagu@decagon.dev>
2022-06-10 13:36:20 +0100

    setup client component and fetched data

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ClientRow from "./ClientRow";
 import { gql, useQuery } from "@apollo/client";
 
 const GET_CLIENTS = gql`
@@ -18,7 +19,27 @@ const Clients = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Something went wrong(</p>;
 
-  return <>{!loading && !error && <h1>Clients</h1>}</>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 
 export default Clients;
