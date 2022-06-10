eb25ff9f48f908f63caa0a5e44d1f9f94c8c8c3c
nwagu chima <chima.nwagu@decagon.dev>
2022-06-10 01:54:12 +0100

    setup sample data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 require("dotenv").config();
+
+const { graphqLHTTP } = require("express-graphql");
+const shcema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+  "/grpahql",
+  graphqlHTTP({
+    shcema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`Server started on port ${port}`));
