98ea6004b3c77650116df4613ca759c67ba7a5fc
nwagu chima <chima.nwagu@decagon.dev>
2022-06-11 09:39:24 +0100

    setup add project mutation

---
+++ client/src/mutations/projectMutation.js
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client";
+
+const ADD_PROJECT = gql`
+  mutation addProject(
+    $name: String!
+    $description: String!
+    $clientId: ID!
+    $status: ProjectStatus!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      clientId: $clientId
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
